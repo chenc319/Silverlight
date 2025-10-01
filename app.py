daddaaaf19731b9c08adaf522d0a1511fd737a38
chenc319 <87655286+chenc319@users.noreply.github.com>
2025-10-01 10:34:57 -0500

    Update app.py

--- app.py
+++ app.py
@@ -88,7 +88,7 @@ if menu == 'Growth & Inflation Model':
 ### ---------------------------------------------------------------------------------------- ###
 
 elif menu == 'GRID Model':
-    app_growth_inflation.plot_grid_factors(start_date, end_date)
+    app_grid.plot_grid_factors(start_date, end_date)
 
 ### ---------------------------------------------------------------------------------------- ###
 ### ------------------------------- YIELD CURVE REGIME MODEL ------------------------------- ###
