2a8d72e9e3615c0f04e35a20dc106c78b31fcb8c
chenc319 <87655286+chenc319@users.noreply.github.com>
2025-10-02 15:41:19 -0500

    Update app_grid.py

--- app_grid.py
+++ app_grid.py
@@ -490,13 +490,13 @@ def grid_z_score_backtest(start, end, **kwargs):
 
     def grid_backtest(row):
         if row['regime_label']== 'Goldilocks':
-            return 1
-        elif row['regime_label']== 'Reflation':
             return 0.75
+        elif row['regime_label']== 'Reflation':
+            return 1
         elif row['regime_label']== 'Deflation':
-            return 0.50
+            return -0.5
         elif row['regime_label']== 'Stagflation':
-            return 0.25
+            return 0.5
         else:
             return np.nan
 
