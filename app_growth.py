6fc56336f8536bb49777584f89b0eef0194987a1
chenc319 <87655286+chenc319@users.noreply.github.com>
2025-10-05 19:24:44 -0500

    Update app_growth.py

--- app_growth.py
+++ app_growth.py
@@ -58,7 +58,7 @@ def growth_inflation_model():
         df[f"{col}_max6"] = df[col].rolling(6).max()
         df[f"{col}_zscore12"] = (df[col] - df[col].rolling(60).mean()) / df[col].rolling(12).std()
 
-    merge_df = merge_dfs([growth.pct_change().shift(-1), df]).dropna()
+    merge_df = merge_dfs([growth.diff().shift(-1), df]).dropna()
 
     X = merge_df.iloc[:, 1:]
     y = merge_df.iloc[:, 0]
