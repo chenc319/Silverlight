280ba62cb571f3bb4e5726d23a9c3bd7b333463b
chenc319 <87655286+chenc319@users.noreply.github.com>
2025-10-07 18:02:00 -0500

    Update app_growth.py

--- app_growth.py
+++ app_growth.py
@@ -41,7 +41,7 @@ def plot_growth_predictor():
     target_feature_df.columns
     # --- Model Setup ---
     result_factor = []
-    window = 36  # Rolling window
+    window = 12  # Rolling window
     factor_features = ['RETAILSMSA', 'USALOLITOAASTSAM', 'INDPRO', 'PCEDG','TOTRESNS','M2SL']
 
 
