25bfe69ea85745a67725524e3946f799b14d15b7
chenc319 <87655286+chenc319@users.noreply.github.com>
2025-11-05 09:46:38 -0600

    Update app_grid.py

--- app_grid.py
+++ app_grid.py
@@ -481,6 +481,7 @@ def grid_mags_backtest():
     grid_growth_inflation_mags['cumsum_bt'] = grid_growth_inflation_mags['bt_returns'].cumsum()
 
     # Drawdown calculations using your helper
+    grid_growth_inflation_mags = grid_growth_inflation_mags.dropna()
     grid_growth_inflation_mags['drawdown_bt'] = compute_drawdown(grid_growth_inflation_mags['cumsum_bt'])
     grid_growth_inflation_mags['drawdown_mags'] = compute_drawdown(grid_growth_inflation_mags['cumsum_mags'])
 
