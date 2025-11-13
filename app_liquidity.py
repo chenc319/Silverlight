739b7df7b5c407f633498f5e080c445ba00b9b9b
chenc319 <87655286+chenc319@users.noreply.github.com>
2025-11-13 16:58:12 -0600

    Update app_liquidity.py

--- app_liquidity.py
+++ app_liquidity.py
@@ -27,7 +27,27 @@ spx_sectors = {
 ### ----------------------------------------------- LIQUIDITY ------------------------------------------------ ###
 ### ---------------------------------------------------------------------------------------------------------- ###
 
-### DATA PULL ###
+### SPX DATA ###
+with open(Path(DATA_DIR) / 'SPX.csv', 'rb') as file:
+    sp500 = pd.read_csv(file)
+sp500.index = pd.to_datetime(sp500['Date']).values
+sp500.drop('Date', axis=1, inplace=True)
+spx_weekly = pd.DataFrame(sp500['Close']).resample('W-FRI').last()
+spx_weekly.columns = ['spx']
+spx_monthly = pd.DataFrame(sp500['Close']).resample('ME').last()
+spx_monthly.columns = ['spx']
+
+### BONDS DATA ###
+with open(Path(DATA_DIR) / 'AGG.csv', 'rb') as file:
+    bonds = pd.read_csv(file)
+bonds.index = pd.to_datetime(bonds['Date']).values
+bonds_weekly = pd.DataFrame(bonds['Close']).resample('W-FRI').last()
+bonds_monthly = pd.DataFrame(bonds['Close']).resample('ME').last()
+
+with open(Path(DATA_DIR) / 'mock_mags_monthly_pct.pkl', 'rb') as file:
+    mock_mags_monthly_pct = pd.read_pickle(file)
+
+### LIQUIDITY ###
 with open(Path(DATA_DIR) / 'treasury.pkl', 'rb') as file:
     treasury = pd.read_pickle(file)
 with open(Path(DATA_DIR) / 'reserves.pkl', 'rb') as file:
@@ -48,10 +68,12 @@ with open(Path(DATA_DIR) / 'dvp_volume_df.pkl', 'rb') as file:
 liquidity_df = merge_dfs([treasury,reserves,tga,rrp_volume]).dropna()
 liquidity_df.index = liquidity_df.index.values
 liquidity_df.columns = ['treasury','reserves','tga','onrrp']
+liquidity_df = liquidity_df.resample('ME').last()
 
 repo_venues_df = merge_dfs([tri_volume_df,gcf_volume_df,dvp_volume_df]).dropna()
 repo_venues_df.index = repo_venues_df.index.values
 repo_venues_df.columns = ['tri','gcf','dvp']
+repo_venues_df = repo_venues_df.resample('ME').last()
 
 ### ---------------------------------------------------------------------------------------------------------- ###
 ### ----------------------------------------------- LIQUIDITY ------------------------------------------------ ###
@@ -89,3 +111,4 @@ def plot_fed_plumbing():
                    colors_array=['#f8b62d', '#f8772d', '#2f90c5'],
                    graph_title='Repo Venues 2nd ROC',
                    y_axis_label='Dollars')
+
