98cd9a3cd59cc431f23911f279c3c5ee2c1c1f8d
chenc319 <87655286+chenc319@users.noreply.github.com>
2025-11-19 16:18:53 -0600

    Update app_prometheus.py

--- app_prometheus.py
+++ app_prometheus.py
@@ -63,7 +63,7 @@ regime_archetypes = {
 asset_cols = ['spx', 'bonds', 'bcom']
 
 ### Z SCORE CALCULATION ###
-window = 12
+window = 36
 df = cross_asset_monthly_merge.pct_change().dropna()
 df_rolling_mean = df.rolling(window).mean()
 df_rolling_std = df.rolling(window).std()
@@ -93,7 +93,7 @@ regime_backtest['bonds_bt'] = np.nan
 regime_backtest['bcom_bt'] = np.nan
 for row in regime_backtest.index:
     if regime_backtest.loc[row, 'closest_regime'] == 'Goldilocks':
-        regime_backtest.loc[row,'equity_bt'] = regime_backtest.loc[row,'spx'] * 1.2
+        regime_backtest.loc[row,'equity_bt'] = regime_backtest.loc[row,'spx'] * 1
         regime_backtest.loc[row,'bonds_bt'] = regime_backtest.loc[row,'bonds'] * 0.8
         regime_backtest.loc[row,'bcom_bt'] = regime_backtest.loc[row,'bcom'] * 0.6
     elif regime_backtest.loc[row, 'closest_regime'] == 'Reflation':
