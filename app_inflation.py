508c53ecbdb8dec5cd8b676f7163319b3e762106
chenc319 <87655286+chenc319@users.noreply.github.com>
2025-11-24 13:16:39 -0600

    Update app_inflation.py

--- app_inflation.py
+++ app_inflation.py
@@ -32,7 +32,7 @@ def plot_inflation_predictor():
     with open(Path(DATA_DIR) / 'm2_money_supply.pkl', 'rb') as file:
         m2_money_supply = pd.read_pickle(file)
     inflation_variables_merge = merge_dfs([inflation_variables_merge,di_reserves,m2_money_supply])
-    target_feature_df = inflation_variables_merge.pct_change(12)
+    target_feature_df = inflation_variables_merge.pct_change(12).diff(3)
     target_feature_df['TOTRESNS'] = target_feature_df['TOTRESNS'] * -1
     target_feature_df['M2SL'] = target_feature_df['M2SL'] * -1
     target_feature_df['CPIAUCSL'] = target_feature_df['CPIAUCSL'].shift(-1)
