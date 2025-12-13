0c615b3d55bf9cfe1c4539498fdcafb3f00d7841
chenc319 <87655286+chenc319@users.noreply.github.com>
2025-12-13 10:19:44 -0600

    Update app_momo.py

--- app_momo.py
+++ app_momo.py
@@ -96,6 +96,10 @@ sector_momo_return_metrics = return_metrics(
 )
 sector_momo_return_metrics['Return/Risk']
 
+### ------------------------------------------------------------------------------------ ###
+### --------------------------------------- MOMO --------------------------------------- ###
+### ------------------------------------------------------------------------------------ ###
+
 ### VOLATILITY SCALING ###
 sector_realized_pct = sector_df.resample('ME').last().pct_change(1)
 sector_rolling_vol = sector_realized_pct.rolling(3).std() * (12**0.5)
@@ -118,6 +122,10 @@ sector_vol_scale_return_metrics = return_metrics(
 )
 sector_vol_scale_return_metrics['Return/Risk']
 
+### ------------------------------------------------------------------------------------ ###
+### --------------------------------------- MOMO --------------------------------------- ###
+### ------------------------------------------------------------------------------------ ###
+
 ### CROSS SECTIONAL SECTOR ROTATION MOMENTUM VS SPX ###
 spx_12_pct = pd.DataFrame(sp500.resample('ME').last().pct_change(12)['Close'])
 sector_x_ret = pd.DataFrame()
@@ -133,9 +141,9 @@ sector_excess_ret_mean = sector_x_ret.rolling(12).mean()
 sector_excess_ret_std = sector_x_ret.rolling(12).std()
 sector_excess_ret_z = (sector_x_ret - sector_excess_ret_mean) / sector_excess_ret_std
 
-sector_excess_return_backtest = pd.DataFrame(columns = ['top4','mid4','bottom4','bt_returns'])
+sector_x_spx_backtest = pd.DataFrame(columns = ['top4','mid4','bottom4','bt_returns'])
 for row in sector_excess_ret_z.index:
-    subset = sectors_12_pct.loc[row].sort_values(ascending=False)
+    subset = sector_excess_ret_z.loc[row].sort_values(ascending=False)
     top4 = subset.index[:4]
     mid3 = subset.index[4:7]
     bottom3 = subset.index[7:11]
@@ -144,19 +152,63 @@ for row in sector_excess_ret_z.index:
     future_bottom4 = sectors_lag_pct.loc[row,bottom3].mean() * 0.15
     total_bt_returns = future_top4 + future_mid3 + future_bottom4
 
-    sector_excess_return_backtest.loc[row, 'top4'] = future_top4
-    sector_excess_return_backtest.loc[row, 'mid4'] = future_mid3
-    sector_excess_return_backtest.loc[row, 'bottom4'] = future_bottom4
-    sector_excess_return_backtest.loc[row, 'bt_returns'] = total_bt_returns
+    sector_x_spx_backtest.loc[row, 'top4'] = future_top4
+    sector_x_spx_backtest.loc[row, 'mid4'] = future_mid3
+    sector_x_spx_backtest.loc[row, 'bottom4'] = future_bottom4
+    sector_x_spx_backtest.loc[row, 'bt_returns'] = total_bt_returns
 
-sector_excess_return_backtest['sector_benchmark'] = sectors_lag_pct.mean(axis=1)
+sector_x_spx_backtest['sector_benchmark'] = sectors_lag_pct.mean(axis=1)
 
-sector_cross_section_return_metrics = return_metrics(
-    sector_excess_return_backtest[['bt_returns','sector_benchmark']],
-    sector_excess_return_backtest[['sector_benchmark']],
+sector_x_spx_return_metrics = return_metrics(
+    sector_x_spx_backtest[['bt_returns','sector_benchmark']],
+    sector_x_spx_backtest[['sector_benchmark']],
     12
 )
-sector_cross_section_return_metrics['Return/Risk']
+sector_x_spx_return_metrics['Return/Risk']
+
+### ------------------------------------------------------------------------------------ ###
+### --------------------------------------- MOMO --------------------------------------- ###
+### ------------------------------------------------------------------------------------ ###
+
+### CROSS SECTIONAL SECTOR ROTATION MOMENTUM VS SPX ###
+bcom_12_pct = pd.DataFrame(bcom.resample('ME').last().pct_change(12)['Close'])
+sector_x_ret = pd.DataFrame()
+for each_sector in sectors_12_pct.columns:
+    merge_df = merge_dfs([sectors_12_pct[each_sector],
+                          bcom_12_pct,
+                          sectors_lag_pct[each_sector]]).dropna()
+    merge_df.columns = ['sector','bcom','lag_sector']
+    merge_df['excess_return'] = merge_df['sector'] - merge_df['bcom']
+    sector_x_ret[each_sector] = merge_df['excess_return']
+
+sector_excess_ret_mean = sector_x_ret.rolling(12).mean()
+sector_excess_ret_std = sector_x_ret.rolling(12).std()
+sector_excess_ret_z = (sector_x_ret - sector_excess_ret_mean) / sector_excess_ret_std
+
+sector_x_bcom_backtest = pd.DataFrame(columns = ['top4','mid4','bottom4','bt_returns'])
+for row in sector_excess_ret_z.index:
+    subset = sector_excess_ret_z.loc[row].sort_values(ascending=False)
+    top4 = subset.index[:4]
+    mid3 = subset.index[4:7]
+    bottom3 = subset.index[7:11]
+    future_top4 = sectors_lag_pct.loc[row,top4].mean() * 0.5
+    future_mid3 = sectors_lag_pct.loc[row,mid3].mean() * 0.35
+    future_bottom4 = sectors_lag_pct.loc[row,bottom3].mean() * 0.15
+    total_bt_returns = future_top4 + future_mid3 + future_bottom4
+
+    sector_x_bcom_backtest.loc[row, 'top4'] = future_top4
+    sector_x_bcom_backtest.loc[row, 'mid4'] = future_mid3
+    sector_x_bcom_backtest.loc[row, 'bottom4'] = future_bottom4
+    sector_x_bcom_backtest.loc[row, 'bt_returns'] = total_bt_returns
+
+sector_x_bcom_backtest['sector_benchmark'] = sectors_lag_pct.mean(axis=1)
+
+sector_x_bcom_return_metrics = return_metrics(
+    sector_x_bcom_backtest[['bt_returns','sector_benchmark']],
+    sector_x_bcom_backtest[['sector_benchmark']],
+    12
+)
+sector_x_bcom_return_metrics['Return/Risk']
 
 ### ------------------------------------------------------------------------------------ ###
 ### --------------------------------------- MOMO --------------------------------------- ###
@@ -183,12 +235,12 @@ def sector_vol_scale_results():
     streamlit_return_metrics_table(sector_vol_scale_return_metrics)
 
 def sector_excess_return_results():
-    streamlit_plot(df=(1 + sector_excess_return_backtest[['bt_returns',
+    streamlit_plot(df=(1 + sector_x_spx_backtest[['bt_returns',
                                             'sector_benchmark']]
                        ).cumprod() - 1,
                    columns_array=['bt_returns', 'sector_benchmark'],
                    colors_array=["#8B0000", "#000000"],
                    graph_title='Equities Historical Performance',
                    y_axis_label='%')
-    streamlit_return_metrics_table(sector_cross_section_return_metrics)
+    streamlit_return_metrics_table(sector_x_spx_return_metrics)
 
